a068a18e6b577004ddd3b64dcc7289df9ec24167
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-05-07 00:54:09 -0600

    deploy

--- dist/client.js
+++ dist/client.js
@@ -29,6 +29,18 @@ function createServerNetworkInterface(baseUrl) {
         console.error("Error closing ably", e);
       }
     },
+    getLatestSnapshot: async (args) => {
+      const response = await fetch(new Request(baseUrl + "/getLatestSnapshot", {
+        method: "POST",
+        body: JSON.stringify(args)
+      }));
+      if (!response.ok) {
+        console.error("Error getting latest snapshot", response);
+        throw new Error("Error getting latest snapshot");
+      }
+      const result = await response.json();
+      return result;
+    },
     subscribeToPoke: (spaceId, listener) => {
       console.log("subscribing to poke", spaceId);
       const channel = ably.channels.get(spaceId);
@@ -49,13 +61,10 @@ function createServerNetworkInterface(baseUrl) {
       };
     },
     push: async (args) => {
-      const response = await fetch(baseUrl + "/push", {
+      const response = await fetch(new Request(baseUrl + "/push", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
         body: JSON.stringify(args)
-      });
+      }));
       if (!response.ok) {
         console.error("Error pushing", response);
         throw new Error("Error pushing");
@@ -64,17 +73,28 @@ function createServerNetworkInterface(baseUrl) {
       console.log("pushed", result);
       return result;
     },
+    createSnapshot: async (args) => {
+      const response = await fetch(new Request(baseUrl + "/createSnapshot", {
+        method: "POST",
+        body: JSON.stringify(args)
+      }));
+      if (!response.ok) {
+        console.error("Error creating snapshot", response);
+        throw new Error("Error creating snapshot");
+      }
+      const result = await response.json();
+      console.log("created snapshot", result);
+      return result;
+    },
     pull: async (args) => {
       console.log("pulling", args, baseUrl);
-      const response = await fetch(baseUrl + "/pull", {
+      const response = await fetch(new Request(baseUrl + "/pull", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
         body: JSON.stringify(args)
-      });
+      }));
       if (!response.ok) {
-        console.error("Error pulling", response);
+        const text = await response.text();
+        console.error("Error pulling", response, text);
         throw new Error("Error pulling");
       }
       const result = await response.json();
@@ -84,63 +104,141 @@ function createServerNetworkInterface(baseUrl) {
   };
 }
 
-// client.ts
-function setup(options) {
-  const { reducer, initialState, spaceId: spaceIdOption, baseUrl } = options;
-  const networkInterface = options.networkInterface ?? createServerNetworkInterface(baseUrl ?? "https://poe-synced-reducer.fly.dev");
-  let state = initialState;
-  const confirmedActions = [];
-  const clientId = crypto.randomUUID();
-  const unconfirmedActions = [];
-  const spaceId = spaceIdOption ?? `reducer` + simpleHash(reducer.toString());
-  const listeners = /* @__PURE__ */ new Set();
-  function getStateWithUnconfirmedActions() {
-    let newState = state;
-    for (const action of unconfirmedActions) {
-      newState = reducer(newState, action.action);
+// reducer_core.ts
+var ReducerCore = class {
+  constructor(reducer, initialState, onStateChange) {
+    this.reducer = reducer;
+    this.initialState = initialState;
+    this.onStateChange = onStateChange;
+    this.state = initialState;
+    this.onStateChange = onStateChange;
+  }
+  state;
+  confirmedActions = [];
+  unconfirmedActions = [];
+  processPullResult(result) {
+    this.processActions(result.actions);
+  }
+  processSnapshot(result) {
+    this.state = result.state;
+    const actionsToProcess = this.#mergeActions(result.actionsSinceLastSnapshot, this.confirmedActions);
+    this.confirmedActions = [];
+    this.processActions(actionsToProcess);
+  }
+  #mergeActions(groundTruth, currentActions) {
+    const mergedActions = groundTruth.slice();
+    for (const action of currentActions) {
+      if (action.serverActionId === mergedActions.at(-1).serverActionId + 1) {
+        mergedActions.push(action);
+      }
+    }
+    return mergedActions;
+  }
+  // returns true if the actions should be pulled
+  shouldPull(actions) {
+    if (actions.length === 0) {
+      return true;
+    }
+    const lastAction = actions.at(-1);
+    if (!lastAction) {
+      return true;
+    }
+    if (!this.getHighestConfirmedActionId()) {
+      return true;
+    }
+    if (this.getHighestConfirmedActionId() === lastAction.serverActionId - 1) {
+      return true;
+    }
+    return false;
+  }
+  getConfirmedState() {
+    return this.state;
+  }
+  getHighestConfirmedActionId() {
+    return this.confirmedActions.at(-1)?.serverActionId;
+  }
+  addUnconfirmedAction(action) {
+    this.unconfirmedActions.push(action);
+    this.onStateChange(this.getState());
+  }
+  processActions(actions) {
+    for (const action of actions) {
+      this.confirmedActions.push(action);
+      const index = this.unconfirmedActions.findIndex((unconfirmedAction) => unconfirmedAction.clientActionId === action.clientActionId);
+      if (index !== -1) {
+        this.unconfirmedActions.splice(index, 1);
+      }
+      this.state = this.reducer(this.state, action.action);
+    }
+    this.onStateChange(this.getState());
+  }
+  getState() {
+    let newState = this.state;
+    for (const action of this.unconfirmedActions) {
+      newState = this.reducer(newState, action.action);
     }
     return newState;
   }
-  function notifyListeners() {
-    const newState = getStateWithUnconfirmedActions();
-    console.table({
-      clientId,
-      spaceId,
-      state,
-      confirmedActions: confirmedActions.length,
-      unconfirmedActions: unconfirmedActions.length,
-      newState
-    });
-    listeners.forEach((listener) => listener(newState));
-  }
-  const clientActionIdToStatus = {};
-  networkInterface.subscribeToPoke(spaceId, (data) => {
-    console.log("received poke", data);
-    if (data.type === "actions") {
-      processActions(data.actions);
-    }
+};
+
+// client.ts
+function setup(options) {
+  const { spaceId: spaceIdOption, baseUrl } = options;
+  const listeners = /* @__PURE__ */ new Set();
+  const core = new ReducerCore(options.reducer, options.initialState, (state) => {
+    listeners.forEach((listener) => listener(state));
   });
-  function processActions(actions) {
-    console.log("processing actions", actions);
-    for (const action of actions) {
-      console.log("processing action", action);
-      confirmedActions.push(action);
-      state = reducer(state, action.action);
-      console.log("state", state);
-      const index = unconfirmedActions.findIndex((unconfirmedAction) => action.clientActionId === unconfirmedAction.clientActionId);
-      if (index !== -1) {
-        unconfirmedActions.splice(index, 1);
+  const networkInterface = options.networkInterface ?? createServerNetworkInterface(baseUrl ?? "https://poe-synced-reducer.fly.dev");
+  const clientId = crypto.randomUUID();
+  const spaceId = spaceIdOption ?? `reducer` + simpleHash(options.reducer.toString());
+  const clientActionIdToStatus = {};
+  const schedulePull = throttle(() => {
+    return networkInterface.pull({ spaceId, lastActionId: core.getHighestConfirmedActionId() ?? -1 }).then((result) => {
+      core.processPullResult(result);
+    });
+  }, 500);
+  const readyPromise = networkInterface.getLatestSnapshot({ spaceId }).then((result) => {
+    core.processSnapshot(result);
+  }).catch((e) => {
+    console.error("error while waiting for initial state", e);
+    return true;
+  });
+  function createSnapshot() {
+    const lastActionId = core.getHighestConfirmedActionId() ?? -1;
+    if (lastActionId > 50) {
+      return networkInterface.createSnapshot({
+        spaceId,
+        lastActionId,
+        state: core.getConfirmedState()
+      });
+    }
+  }
+  networkInterface.subscribeToPoke(spaceId, async (data) => {
+    console.log("received poke", data);
+    await readyPromise;
+    if (data.type === "actions") {
+      const lastAction = data.actions.at(-1);
+      if (!lastAction) {
+        console.error("received poke with no actions");
+        return;
+      }
+      if (!core.getHighestConfirmedActionId() || core.getHighestConfirmedActionId() === lastAction.serverActionId - 1) {
+        core.processActions(data.actions);
+        const lastActionId = core.getHighestConfirmedActionId() ?? -1;
+        if (Math.random() > 0.99 && lastActionId > 50) {
+          networkInterface.createSnapshot({
+            spaceId,
+            lastActionId,
+            state: core.getConfirmedState()
+          });
+        }
+      } else {
+        schedulePull();
       }
     }
-    notifyListeners();
-  }
-  const readyPromise = networkInterface.pull({ spaceId, lastActionId: -1 }).then((result) => {
-    console.log("pulled actions", result);
-    processActions(result.actions);
   });
   const pushActions = throttle(async () => {
-    const actionsToFlush = unconfirmedActions.filter((action) => clientActionIdToStatus[action.clientActionId] === "waiting");
-    console.log("pushing", actionsToFlush);
+    const actionsToFlush = core.unconfirmedActions.filter((action) => clientActionIdToStatus[action.clientActionId] === "waiting");
     actionsToFlush.forEach((action) => {
       clientActionIdToStatus[action.clientActionId] = "pending";
     });
@@ -150,27 +248,24 @@ function setup(options) {
     clientId,
     networkInterface,
     isReady: async () => {
-      await readyPromise.catch((e) => {
-        console.error("error while waiting for initial state", e);
-        return false;
-      });
+      await readyPromise;
       return true;
     },
     subscribe: (listener) => {
       listeners.add(listener);
-      listener(state);
+      listener(core.getState());
       return () => {
         listeners.delete(listener);
       };
     },
     dispatch: (action) => {
       const clientActionId = crypto.randomUUID();
-      unconfirmedActions.push({ action, clientActionId });
+      core.addUnconfirmedAction({ action, clientActionId });
       clientActionIdToStatus[clientActionId] = "waiting";
-      notifyListeners();
       pushActions();
     },
-    getState: () => getStateWithUnconfirmedActions()
+    getState: () => core.getState(),
+    createSnapshot
   };
 }
 function throttle(fn, delay) {
