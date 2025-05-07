32aecdaf20ef13a33371f9bf4e047ccb36a32f60
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-05-07 01:02:39 -0600

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -2,3 +2,9 @@ sh ./_build.sh
 git add -A
 git commit -m "deploy"
 git push
+
+
+echo "https://www.val.town/x/jeffreyyoung/synced_reducer/code/main.tsx"
+
+CURRENT_COMMIT=$(git rev-parse HEAD)
+echo "latest commit: $CURRENT_COMMIT"
